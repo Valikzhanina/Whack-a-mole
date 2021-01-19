0f0a47e1174b8c36e4133b534e8932bc9547f1b2
1 <a1@1s-MacBook-Pro.local>
2021-01-19 19:42:27 +0200

    norm functionality of color td

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function getTimer() {
   select.addEventListener('change', () => {
     let timer = select.value;
     let delay = +timer * 1000;
-    console.log(delay);
     createPlayers();
     play(td, delay);
 
@@ -18,13 +17,14 @@ getTimer();
 
 const result = document.querySelector('div');
 let flag = false;
+
+let userCounter = 0;
+let computerCounter = 0;
 function createPlayers() {
   if (flag) return console.log('add recently');
   const user = document.createElement('div');
-  let userCounter = 0;
   user.textContent = `User : ${userCounter}`;
   const computer = document.createElement('div');
-  let computerCounter = 0;
   computer.textContent = `Computer : ${computerCounter}`;
   result.append(user, computer);
   flag = true;
@@ -36,15 +36,35 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 let randomNumber = getRandomIntInclusive(1, 100);
-console.log(randomNumber);
+
 function play(td, delay) {
   setTimeout(function color() {
     randomNumber = getRandomIntInclusive(1, 100);
     for (let i = 0; i <= td.length; i++) {
       if (i == randomNumber - 1) {
         td[i].style.backgroundColor = 'red';
+
+        let change = setTimeout(() => {
+          td[i].style.backgroundColor = 'blue';
+          computerCounter++;
+          console.log(computerCounter);
+        }, delay);
+
+        td[i].addEventListener('click', () => {
+          clearTimeout(change);
+          if (td[i].style.backgroundColor == 'red') {
+            td[i].style.backgroundColor = 'green';
+            userCounter++;
+            console.log(userCounter);
+          } else {
+            td[i].style.backgroundColor = 'blue';
+            // computerCounter++;
+            console.log(computerCounter);
+          }
+        });
+
         getRandomIntInclusive(1, 100);
-        console.log(getRandomIntInclusive(1, 100));
+        // console.log(getRandomIntInclusive(1, 100));
         setTimeout(color, delay);
       }
     }
