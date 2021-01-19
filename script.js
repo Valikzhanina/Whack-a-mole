1693db7de515458dc4353c9a538d1bab351bf72c
1 <a1@1s-MacBook-Pro.local>
2021-01-19 15:46:02 +0200

    add colored function

--- script.js
+++ script.js
@@ -2,17 +2,16 @@ const td = document.querySelectorAll('td');
 const level = document.querySelectorAll('option');
 const select = document.querySelector('select');
 let t;
+
 function getTimer() {
   select.addEventListener('change', () => {
     let timer = select.value;
-    console.log(timer);
     createPlayers();
+    play(td, timer);
     return timer;
   });
 }
-
-let c = getTimer();
-console.log(c);
+getTimer();
 
 const result = document.querySelector('div');
 let flag = false;
@@ -33,16 +32,16 @@ function getRandomIntInclusive(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-let randomNumber = getRandomIntInclusive(1, 100);
-console.log(randomNumber);
+let randomNumber = getRandomIntInclusive(0, 99);
+// console.log(randomNumber);
 
-// if (select.value !== 'undefined') {
-//   setInterval(function () {
-//     for (let elem of td) {
-//       if (elem === getRandomIntInclusive(0, 100) - 1) {
-//         console.log('ghbdtn');
-//         td[elem].style.backgroundColor = 'blue';
-//       }
-//     }
-//   }, 300);
-// }
+function play(td) {
+  for (let i = 0; i <= td.length; i++) {
+    if (i == randomNumber) {
+      setTimeout(() => {
+        td[i].style.backgroundColor = 'red';
+        console.log('dfghjhgfdfgh');
+      }, 3000);
+    }
+  }
+}
