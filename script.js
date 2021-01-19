1ab3c2a2d286eeb9252b75542bdeec01f443b254
1 <a1@1s-MacBook-Pro.local>
2021-01-19 13:56:32 +0200

    add players

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function getTimer() {
   select.addEventListener('change', () => {
     let timer = select.value;
     console.log(timer);
+    createPlayers();
     return timer;
   });
 }
@@ -14,58 +15,34 @@ let c = getTimer();
 console.log(c);
 
 const result = document.querySelector('div');
-let userCounter = 0;
-let computerCounter = 0;
+let flag = false;
+function createPlayers() {
+  if (flag) return console.log('add recently');
+  const user = document.createElement('div');
+  let userCounter = 0;
+  user.textContent = `User : ${userCounter}`;
+  const computer = document.createElement('div');
+  let computerCounter = 0;
+  computer.textContent = `Computer : ${computerCounter}`;
+  result.append(user, computer);
+  flag = true;
+}
+
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+let randomNumber = getRandomIntInclusive(1, 100);
+console.log(randomNumber);
 
-if (select.value !== 'undefined') {
-  setInterval(function () {
-    for (let elem of td) {
-      if (elem === getRandomIntInclusive(0, 100) - 1) {
-        console.log('ghbdtn');
-        td[elem].style.backgroundColor = 'blue';
-      }
-    }
-  }, 300);
-}
-
-// coloredRandom();
-// function coloredRandom() {
-// getRandomIntInclusive(1, 100);
-
-// getRandomIntInclusive(1, 100);
-// setTimeout(coloredRandom, time);
-// setTimeout(coloredRandom, 1000);
-// function colorRandom() {
-//   for (let i = 1; i <= td.length; i++) {
-//     if (i === getRandomIntInclusive(1, 100)) {
-//       td[i].style.backgroundColor = 'blue';
+// if (select.value !== 'undefined') {
+//   setInterval(function () {
+//     for (let elem of td) {
+//       if (elem === getRandomIntInclusive(0, 100) - 1) {
+//         console.log('ghbdtn');
+//         td[elem].style.backgroundColor = 'blue';
+//       }
 //     }
-//   }
-
-//       td[i].addEventListener('click', function (e) {
-//         e.target.style.backgroundColor = 'green';
-//         userCounter++;
-//         user = document.createElement('div');
-//         user.textContent = userCounter;
-//         result.appendChild(user);
-//         counter++;
-//       });
-//       function colored() {
-//         td[i].style.backgroundColor = 'red';
-//         computerCounter++;
-//       //   if (counter > 0) {
-//       //     td[i].addEventListener('click', function (e) {
-//       //       e.target.style.backgroundColor = 'green';
-//       //     });
-//       //   }
-//       setTimeout(colored, 1500);
-//     }
-//     setTimeout(colored, 1500);
-//   }
-
+//   }, 300);
 // }
